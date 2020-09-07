3de9cee1363af2844791df4dd50172e405087a21
Mary Schmidt <maryschmidt1982@gmail.com>
2020-09-07 18:56:27 -0500

    revisions

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-function handleImageClicks() {
-  $(".thumbnail").on("click", function (event) {
-    const imgSrc = $(event.currentTarget).find("img").attr("src");
-    const imgAlt = $(event.currentTarget).find("img").attr("alt");
-    $(".carousel.img").attr("src", imgSrc).attr("alt", imgAlt);
+const photos = [
+  "images/clouds1.jpg",
+  "images/clouds2.jpg",
+  "images/clouds3.jpg",
+  "images/clouds4.jpg",
+];
+var imageCounter = 0;
+
+function handleNextClick() {
+  $("#next").on("click", function (event) {
+    if (imageCounter < photos.length) {
+      $("#clouds").attr("src", photos[imageCounter]);
+      imageCounter = imageCounter + 1;
+    } else {
+      imageCounter = 0;
+    }
   });
 }
 
-$(handleImageClicks);
+//add a previous button
+//decrease the imageCounter
+
+function handlePreviousClick() {
+  $("#previous").on("click", function (event) {
+    if (imageCounter < photos.length) {
+      $("#clouds").attr("src", photos[imageCounter]);
+      imageCounter = imageCounter -= 1;
+    } else {
+      imageCounter = 0;
+    }
+  });
+}
+
+$(handleNextClick);
